aefd2b019323889e4ad04488feb44cc74d93f149
Vanessa Toro <vanetoro84@gmail.com>
2018-01-15 18:45:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ function logWhisper(string){
 
 function sayHiToGrandma(string){
   if(string.toLowerCase()){
-    return "I can't hear you!"
+    return `I can't hear you!`
   } else 
-    if(string === 'I love you, Grandma.') {
-      return 'I love you, too.'
+    if(string === `I love you, Grandma.`) {
+      return `I love you, too.`
     } else 
       if(string.toUpperCase()) {
     return "YES INDEED"
